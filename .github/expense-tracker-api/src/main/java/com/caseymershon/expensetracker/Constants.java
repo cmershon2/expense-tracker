3b3fac34a5aa4d296db3dc4fa62984a8800b8aa7
Casey Mershon <caseymershon@gmail.com>
2021-07-08 22:58:08 -0400

    Init_Reverted_Secrets

--- .github/expense-tracker-api/src/main/java/com/caseymershon/expensetracker/Constants.java
+++ .github/expense-tracker-api/src/main/java/com/caseymershon/expensetracker/Constants.java
@@ -2,7 +2,7 @@ package com.caseymershon.expensetracker;
 
 public class Constants {
     
-    public static final String API_SECRET_KEY = System.getenv("API_SECRET_KEY");
+    public static final String API_SECRET_KEY = "KYCwUoFk0d1Rp2RlQ7kFhXaMfvZiEclj";
 
     public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
     
